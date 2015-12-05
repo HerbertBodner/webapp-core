d142f6d0d6329231be2668b57a96974d79d01af2
Herbert Bodner <herbert.bodner@gmail.com>
2015-12-05 12:05:08 +0100

    PasswordService CheckStrength of password

---
+++ src/WaCore.Bl/Services/PasswordService.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WaCore.Bl.Services
+{
+    public class PasswordService
+    {
+        public enum PasswordScore
+        {
+            Blank = 0,
+            TooShort = 1,
+            RequirementsNotMet = 2,
+            VeryWeak = 3,
+            Weak = 4,
+            Fair = 5,
+            Medium = 6,
+            Strong = 7,
+            VeryStrong = 8
+        }
+
+        public static PasswordScore CheckStrength(string password)
+        {
+            int score = 0;
+
+            // using three requirements here:  min length and two types of characters (numbers and letters)
+            bool blnMinLengthRequirementMet = false;
+            bool blnRequirement1Met = false;
+            bool blnRequirement2Met = false;
+
+            // check for chars in password
+            if (password.Length < 1)
+                return PasswordScore.Blank;
+
+            // if less than 6 chars, return as too short, else, plus one
+            if (password.Length < 6)
+            {
+                return PasswordScore.TooShort;
+            }
+            else
+            {
+                score++;
+                blnMinLengthRequirementMet = true;
+            }
+
+            // if 8 or more chars, plus one
+            if (password.Length >= 8)
+                score++;
+
+            // if 10 or more chars, plus one
+            if (password.Length >= 10)
+                score++;
+
+            // if 12 or more chars, plus one
+            if (password.Length >= 12)
+                score++;
+
+            // if password has a number, plus one
+            if (Regex.IsMatch(password, @"[\d]", RegexOptions.ECMAScript))
+            {
+                score++;
+                blnRequirement1Met = true;
+            }
+
+            // if password has lower case letter, plus one
+            if (Regex.IsMatch(password, @"[a-z]", RegexOptions.ECMAScript))
+            {
+                score++;
+                blnRequirement2Met = true;
+            }
+
+            // if password has upper case letter, plus one
+            if (Regex.IsMatch(password, @"[A-Z]", RegexOptions.ECMAScript))
+            {
+                score++;
+                blnRequirement2Met = true;
+            }
+
+            // if password has a special character, plus one
+            if (Regex.IsMatch(password, @"[~`!@#$%\^\&\*\(\)\-_\+=\[\{\]\}\|\\;:'\""<\,>\.\?\/£]", RegexOptions.ECMAScript))
+                score++;
+
+            // if password is longer than 2 characters and has 3 repeating characters, minus one (to minimum of score of 3)
+            List<char> lstPass = password.ToList();
+            if (lstPass.Count >= 3)
+            {
+                for (int i = 2; i < lstPass.Count; i++)
+                {
+                    char charCurrent = lstPass[i];
+                    if (charCurrent == lstPass[i - 1] && charCurrent == lstPass[i - 2] && score >= 4)
+                    {
+                        score--;
+                    }
+                }
+            }
+
+            if (!blnMinLengthRequirementMet || !blnRequirement1Met || !blnRequirement2Met)
+            {
+                return PasswordScore.RequirementsNotMet;
+            }
+
+            return (PasswordScore)score;
+        }
+    }
+}
---
+++ src/WaCore.UnitTests/Bl/Services/PaswordServiceCheckStrengthTests.cs
@@ -0,0 +1,71 @@
+﻿using WaCore.Bl.Services;
+using Xunit;
+
+namespace WaCore.UnitTests.Bl.Services
+{
+    public class PaswordServiceCheckStrengthTests
+    {
+        [Fact]
+        public void PasswortEmpty_Blank()
+        {
+            Assert.Equal(PasswordService.PasswordScore.Blank, PasswordService.CheckStrength(""));
+        }
+
+        [Fact]
+        public void Passwort_5Chars_TooShort()
+        {
+            Assert.Equal(PasswordService.PasswordScore.TooShort, PasswordService.CheckStrength("12345"));
+        }
+
+        [Fact]
+        public void Passwort_6Digits_RequirementsNotMet()
+        {
+            Assert.Equal(PasswordService.PasswordScore.RequirementsNotMet, PasswordService.CheckStrength("123456"));
+        }
+
+
+        [Fact]
+        public void Passwort_6Chars_Digits_VeryWeak()
+        {
+            Assert.Equal(PasswordService.PasswordScore.VeryWeak, PasswordService.CheckStrength("a234567"));
+        }
+
+
+        [Fact]
+        public void Passwort_6LowerUpperCharsAndDigit_Weak()
+        {
+            Assert.Equal(PasswordService.PasswordScore.Weak, PasswordService.CheckStrength("aA1234"));
+        }
+
+        [Fact]
+        public void Passwort_8LowerUpperCharAndDigit_Fair()
+        {
+            Assert.Equal(PasswordService.PasswordScore.Fair, PasswordService.CheckStrength("Passwor1"));
+        }
+
+        [Fact]
+        public void Passwort_10LowerUpperCharsAndDigit_Medium()
+        {
+            Assert.Equal(PasswordService.PasswordScore.Medium, PasswordService.CheckStrength("Password123"));
+        }
+
+        [Fact]
+        public void Passwort_10LowerUpperCharsAndDigitAndSpecialChar_Strong()
+        {
+            Assert.Equal(PasswordService.PasswordScore.Strong, PasswordService.CheckStrength("Password1#"));
+        }
+
+        [Fact]
+        public void Passwort_12LowerUpperCharsAndDigitAndSpecialChar_VeryStrong()
+        {
+            Assert.Equal(PasswordService.PasswordScore.VeryStrong, PasswordService.CheckStrength("Password123#"));
+        }
+
+        [Fact]
+        public void Passwort_10LowerUpperCharsAndDigitAndSpecialCharAndRepeatingChar_VeryWeak()
+        {
+            Assert.Equal(PasswordService.PasswordScore.VeryWeak, PasswordService.CheckStrength("aA#1111111"));
+        }
+
+    }
+}
