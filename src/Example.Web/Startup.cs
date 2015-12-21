73b82f5284fbfe9c4e1fae1957ec9da01a955264
Herbert Bodner <h.bodner@tech.leaseweb.com>
2015-12-21 22:56:51 +0100

    Create Example.Web project

---
+++ src/Example.Web/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+using WaCore.Web;
+
+namespace Example.Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                //.AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                //builder.AddUserSecrets();
+            }
+
+
+            // most common use of environment variables would be in azure hosting
+            // since it is added last anything in env vars would trump the same setting in previous config sources
+            // so no risk of messing up settings if deploying a new version to azure
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureWaCore(Configuration);
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // Add the platform handler to the request pipeline.
+            app.UseIISPlatformHandler();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
---
+++ src/WaCore.Web/ConfigurationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Controllers;
+using Microsoft.AspNet.Mvc.Razor;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+using WaCore.Web.Infrastructure;
+using Microsoft.Framework.DependencyInjection.Extensions;
+
+namespace WaCore.Web
+{
+    public static class ConfigurationExtensions
+    {
+        public static IServiceCollection ConfigureWaCore(this IServiceCollection services,
+            IConfigurationRoot configuration)
+        {
+            services.TryAddSingleton<IRazorViewEngine, WaCoreRazorViewEngine>();
+            // WaCoreRazorViewEngine adds /Views/WaCore as the last place to search for views
+            // WaCore views are all under Views/WaCore
+            // to modify a view just copy it to a higher priority location
+            // ie copy /Views/WaCore/Manage/*.cshtml up to /Views/Manage/ and that one will have higher priority
+            // and you can modify it however you like
+            // upgrading to newer versions of WaCore could modify or add views below /Views/WaCore
+            // so you may need to compare your custom views to the originals again after upgrades
+
+
+            services.AddMvc();
+            return services;
+        }
+    }
+}
