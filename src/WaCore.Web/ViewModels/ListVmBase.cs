c2fc9b6b2432f696a0dd17269f02d763d9e5d599
Herbert Bodner <h.bodner@tech.leaseweb.com>
2015-12-28 18:29:04 +0100

    Implement generic ListBaseController with ListBaseVm and base SearchConfig object; this controller can be used for listing some entity

---
+++ src/WaCore.Web/ViewModels/ListVmBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaCore.Web.ViewModels
+{
+    public interface IVmList : IVmBase
+    {
+    }
+
+    public class ListVmBase<TDto, TSearchConfig, TFilter> : VmBase, IVmList
+        where TDto : class
+        where TSearchConfig : SearchConfig<TFilter>, new()
+        where TFilter : class
+    {
+        //Does not build for DNXCore
+        //public StaticPagedList<TDto> DtoPagedList { get; set; }
+
+        public TSearchConfig SearchConfig { get; set; }
+
+        protected ListVmBase(string displayName, string displayDescription)
+            : base(displayName, displayDescription)
+        {
+            SearchConfig = new TSearchConfig();
+        }
+    }
+}
