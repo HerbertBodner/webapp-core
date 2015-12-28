0c52b1ef712d6c860dba7c21009f6894565464d0
Herbert Bodner <h.bodner@tech.leaseweb.com>
2015-12-28 18:30:48 +0100

    Implement UserController with Listing page

---
+++ src/WaCore.Web/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaCore.Contracts.Bl.Services;
+using WaCore.Contracts.Data.Filters;
+using WaCore.Contracts.Data.Repositories;
+using WaCore.Contracts.Data.Repositories.Base;
+using WaCore.Entities.Core;
+using WaCore.Web.Controllers.Base;
+using WaCore.Web.ViewModels.Users;
+
+
+namespace WaCore.Web.Controllers
+{
+    public class UserController : ListBaseController<UserListVm, UserSearchConfig, User, UserFilter>
+    {
+        private readonly IUserRepository _userRepo;
+
+        public UserController(IUserRepository userRepo) 
+        {
+            _userRepo = userRepo;
+        }
+
+        protected override Task PrepareListVmDataAsync(UserListVm vm)
+        {
+            var lst = _userRepo.GetByUserFilter(vm.SearchConfig.ToFilter());
+            vm.UserLst = lst.ToList();
+            return Task.FromResult(0);
+        }
+    }
+}
