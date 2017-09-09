1bfccb452b4d9f2af2aab900d4a59dd2f51e4214
Jacek Olearczyk <rurku@rurku.com>
2017-09-09 12:46:45 +0200

    Add repositories as their implemented interfaces

--- src/WaCore.Data.Ef/DependencyInjection/RepositoryConfiguration.cs
+++ src/WaCore.Data.Ef/DependencyInjection/RepositoryConfiguration.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WaCore.Contracts.Data;
 
 namespace WaCore.Data.Ef.DependencyInjection
 {
-    public class RepositoryConfiguration<TDbContext>
+    public class RepositoryConfiguration<TDbContext, TUnitOfWorkService>
+        where TUnitOfWorkService : IWacUnitOfWork
     {
         private readonly IServiceCollection _serviceCollection;
 
@@ -20,12 +22,16 @@ namespace WaCore.Data.Ef.DependencyInjection
         /// <typeparam name="TService">Interface implemented by the repository</typeparam>
         /// <typeparam name="TImplementation">Repository concrete type. It must have a public constructor which takes the DbContext as argument.</typeparam>
         /// <returns></returns>
-        public RepositoryConfiguration<TDbContext> AddRepository<TService, TImplementation>()
+        public RepositoryConfiguration<TDbContext, TUnitOfWorkService> AddRepository<TService, TImplementation>()
+            where TService: class
             where TImplementation : TService
         {
             // Add repositories not as their implemented interfaces but as factory functions which take dbcontext as argument.
             // This allows the UnitOfWork to create repository instances with dbcontext argument provided at runtime.
             _serviceCollection.AddTransient<Func<TDbContext, TService>>(serviceProvider => dbContext => ActivatorUtilities.CreateInstance<TImplementation>(serviceProvider, dbContext));
+
+            // For convinience register repositories as their interfaces as well, but then use the unit of work to create them
+            _serviceCollection.AddTransient<TService>(serviceProvider => serviceProvider.GetRequiredService<TUnitOfWorkService>().GetRepository<TService>());
             return this;
         }
 
