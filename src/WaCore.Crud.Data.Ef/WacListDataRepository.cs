a659855846a7440db0a9e03a17b64f5e85b9fd17
Herbi <herbert.bodner@gmail.com>
2017-09-10 22:15:59 +0200

    Get rid of removed WacDbContext

--- src/WaCore.Crud.Data.Ef/WacListDataRepository.cs
+++ src/WaCore.Crud.Data.Ef/WacListDataRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using WaCore.Crud.Contracts.Data;
 using WaCore.Crud.Dtos.Filters;
 using WaCore.Data;
@@ -8,7 +9,7 @@ namespace WaCore.Crud.Data.Ef
 {
     public abstract class WacListDataRepository<TEntity, TDbContext, TFilter> : WacRepository<TEntity, TDbContext>, IWacListDataRepository<TEntity, TFilter>
         where TEntity : class
-        where TDbContext : WacDbContext
+        where TDbContext : DbContext
         where TFilter : WacFilter
     {
         public WacListDataRepository(TDbContext dbContext) : base(dbContext)
