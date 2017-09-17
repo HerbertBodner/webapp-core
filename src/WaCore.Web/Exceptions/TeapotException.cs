33ba1474d5702332c19623e55f52e4ef88876035
Yury Shchohaleu <y.shchohaleu@global.leaseweb.com>
2017-09-17 13:35:56 +0200

    add web api exceptions

---
+++ src/WaCore.Web/Exceptions/TeapotException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace WaCore.Web.Exceptions
+{
+    public class TeapotException : WebApiException
+    {
+        private const string BaseMessage = "I'm a teapot";
+
+        public TeapotException()
+            : base((HttpStatusCode)418, BaseMessage)
+        {
+        }
+    }
+}
---
+++ src/WaCore.Web/Exceptions/WebApiException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace WaCore.Web.Exceptions
+{
+    public class WebApiException: Exception
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+
+        public WebApiException(HttpStatusCode httpStatusCode, string message) :
+            base(message)
+        {
+            HttpStatusCode = httpStatusCode;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendFormat("Status Code: {0} - {1}", (int)HttpStatusCode, HttpStatusCode.ToString());
+            stringBuilder.Append(Environment.NewLine);
+
+            stringBuilder.AppendFormat(Message);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
