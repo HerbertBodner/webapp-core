f43e6e53b3b45f3abac401577f5d3bc88aed56a4
Jacek Olearczyk <rurku@rurku.com>
2017-09-30 11:29:02 +0200

    Removed unused method SortColumnDescriptor.Apply

--- src/WaCore.Crud.Utils/Sorting/SortFieldMapping.cs
+++ src/WaCore.Crud.Utils/Sorting/SortFieldMapping.cs
@@ -30,14 +30,5 @@ namespace WaCore.Crud.Utils.Sorting
         // This should be of type Expression<Func<TEntity,KeyType>>
         public Expression KeySelector { get; set; }
         public bool Asc { get; set; }
-
-        public IQueryable<TEntity> Apply(IQueryable<TEntity> q, bool reverseOrder)
-        {
-            var asc = Asc ^ reverseOrder;
-            var methodName = asc ? "OrderBy" : "OrderByDescending";
-            var types = new[] { q.ElementType, KeyType };
-            var mce = Expression.Call(typeof(Queryable), methodName, types, q.Expression, KeySelector);
-            return q.Provider.CreateQuery<TEntity>(mce);
-        }
     }
 }
