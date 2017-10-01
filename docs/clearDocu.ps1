8dca22c6eccac105c3734128c6ccfab44e91562c
HerbertBodner <herbert.bodner@gmail.com>
2017-10-01 12:47:20 +0200

    Script to clearDocu

---
+++ docs/clearDocu.ps1
@@ -0,0 +1,9 @@
+﻿
+Remove-Item -recurse -force -ErrorAction SilentlyContinue docs/_site
+Remove-Item -recurse -force -ErrorAction SilentlyContinue docs/obj
+Remove-Item -recurse -force -ErrorAction SilentlyContinue docfx.console.2.24.0/
+Remove-Item -recurse -force -ErrorAction SilentlyContinue api
+Remove-Item -recurse -force -ErrorAction SilentlyContinue origin_site/
+Remove-Item docs/api/*.yml
+Remove-Item docs/api/.manifest -ErrorAction SilentlyContinue
+
